b0ac7242a25d630d9795e119e4eb50ddd7c8f157
thierry.semon <semon@space.unibe.ch>
2017-03-03 13:48:25 +0100

    Creation d'une branche GIT pour le développement du code du projet

--- localhost/test/DB_access.php
+++ localhost/test/DB_access.php
@@ -1,49 +1,33 @@
-<!doctype html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Test DB access</title>
-	</head>
-	<body>
-	<?php
+<?php
+	
+	function db_connect()
+	{
 		try
 		{
 			// connection à base de donnée PDO
-			$db = new PDO ('mysql:host = localhost; dbname = boulangerie', 'root', '');
+			$db = new PDO ('mysql:host = localhost; dbname = boulangerie', 'root', '',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			return $db;
 		}
 		catch (PDOException $e)
 		{
 			// En cas d'erreur: affichage message (et exit?)
 			echo "PDO: " . htmlentities($e->getMessage());
 		}
-		// Récupère contenu de la table produits
-		$result = $db->query('SELECT * FROM boulangerie.produits');
-		
-		// Affichage contenu de la table produits
-		echo '<p><strong>Produits:</strong ></p>'; 
-		while ( $produit = $result->fetch(PDO::FETCH_ASSOC))
-		{
-			echo htmlentities($produit['nom']) . ' coûte : ' . htmlentities($produit['prix']). 'CHF' . '<br />';
-		}
-		
-		//Insertion nouveau produit
-		/*
-		$name = 'Croissant au beurre';
-		$prix = '1.2';
-		$st = $db->prepare('INSERT INTO boulangerie.produits(nom, prix) '. 'VALUES (?, ?)');
-		$st->execute(array($name, $prix));
-		*/
-		
-		//Suppression d'un produit
-		/*
-		$name = 'Croissant au beurre';
-		$st = $db->prepare("DELETE FROM boulangerie.produits WHERE (nom = ?)");
-    if ($st && $st->execute(array($name))) {
-         echo "Produit " . $name . " supprimé !";
-    }*/
-				
-		$result -> closeCursor ();
+	}
+	function validation_utilisateur()
+	{
+		return 1;
+	}
+	
+	function a($u, $t, $a) {
+		$callback = function($x) use ($a) {
+      return urlencode($x) . "=" . urlencode($a[$x]);
+   };
 
-	?>
-  </body>
-</html>
+   return '<a href="'
+          . $u . '?'
+          . join("&amp;", array_map($callback, array_keys($a)))
+	  . '">'
+	  . $t
+	  . '</a>';
+}
