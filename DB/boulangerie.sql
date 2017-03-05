9f0b953c38edc219e93c90598f319c4e0c8c3700
thierry.semon <semon@space.unibe.ch>
2017-03-05 07:50:10 +0100

    Ajout table des commandes

--- DB/boulangerie.sql
+++ DB/boulangerie.sql
@@ -1,35 +1,30 @@
 # mysql < boulangerie.sql
+# forcer le travail en UTF-8
+SET collation_connection = utf8_general_ci;
+SET NAMES 'utf8';
 
-	-- SET NAMES "utf8";
-
-	-- CREATE or replace DATABASE boulangerie;
-	-- USE boulangerie;
-
-	-- CREATE TABLE IF NOT EXISTS
-		 -- produits(id INT AUTO_INCREMENT NOT NULL,
-				-- nom VARCHAR(100) NOT NULL,
-				-- prix NUMERIC(10,2) NOT NULL,
-				-- quantite INTEGER NOT NULL,
-				-- time_stamp DATETIME NOT NULL,
-				-- PRIMARY KEY(id),
-				-- UNIQUE(nom));
-
-	-- INSERT INTO produits(nom, prix) VALUES('Croissant au chocolat', 2.50);
-	-- INSERT INTO produits(nom, prix) VALUES('Croissant au beurre', 1.50);
-
-SET NAMES "utf8";
-
-CREATE DATABASE boulangerie;
+CREATE DATABASE IF NOT EXISTS boulangerie;
 USE boulangerie;
 
 CREATE TABLE produits(id INT AUTO_INCREMENT NOT NULL,
       nom VARCHAR(100) NOT NULL,
       prix NUMERIC(10,2) NOT NULL,
-			quantite INTEGER,
+			quantite INTEGER NOT NULL,
+			quantite_restante INTEGER,
 			time_stamp DATETIME,
       PRIMARY KEY(id),
-      UNIQUE(nom));
-
+      UNIQUE(nom)) ENGINE=InnoDB CHARSET=utf8;
+			
+CREATE TABLE commandes(id INT AUTO_INCREMENT NOT NULL,
+      nom VARCHAR(100) NOT NULL,
+			prenom VARCHAR(100) NOT NULL,
+			entreprise VARCHAR(100) NOT NULL,
+			produit VARCHAR(100) NOT NULL,
+			quantite INTEGER NOT NULL,
+			prix_total NUMERIC(10,2) NOT NULL,
+			time_stamp DATETIME,
+      PRIMARY KEY(id)) ENGINE=InnoDB CHARSET=utf8;
+			
 INSERT INTO produits(nom, prix, quantite) VALUES('Petit pain', 1.30, 3);
 INSERT INTO produits(nom, prix, quantite) VALUES('Croissant au beurre', 1.50, 6);
 INSERT INTO produits(nom, prix, quantite) VALUES('Croissant au chocolat', 2.50, 2);
