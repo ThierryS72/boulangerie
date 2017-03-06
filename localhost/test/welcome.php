e31a2bd02c85400b88707885dbed39d74faa765e
André Mooser MBPro Git <andre@macbook-pro-de-andre-mooser-5-3.home>
2017-03-06 16:28:55 +0100

    Création d'une variable pour les produits commandés avec suppression possible

--- localhost/test/welcome.php
+++ localhost/test/welcome.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); // a placer en tout premier, avant HTML car utilise un cookie
 ini_set('default_charset', 'UTF-8');
 header("Content-Type: text/html; charset=UTF-8");
 
@@ -143,89 +144,84 @@ if(validation_utilisateur())
 					$produitChoisi[$clé] = $valeur;
 				}
 				print_r ($produitChoisi);
+
+				// Suppression de la ligne sélectionnée de l'array des commandes en cours
+				// if (isset($_GET['supprimer']) && is_scalar($_GET['supprimer']))
+				// {
+				// 	unset($commandes);
+				// }
+
 				/* On récupère si il existe le nom du produit commandé par le formulaire */
 				$prodcommande = isset($_POST['nomform'])?$_POST['nomform']:null;
-				echo "<p> Produit commandé: " . $prodcommande . "</p>";
+				$quantitecommande = isset($_POST['quantiteform'])?$_POST['quantiteform']:null;
+				echo "<p> Produit commandé: " . $prodcommande . " , nbre de pièces: " . $quantitecommande . "</p>";
 				?>
 				<form action="<?php echo $url_page ?>" method="post" id="commande">
 					<fieldset style="border: 3px double #333399">
 						<legend>Sélectionnez un produit</legend>
-							<?php
-							echo '<select name="' . urlencode('nomform') . '" onchange="document.forms[\'commande\'].submit();">';
-							foreach($produitChoisi as $key => $value)
-							{
-								echo '<option value="' . htmlentities($key) . '" ' . ((isset($prodcommande) && $prodcommande == $key)?" selected=\"selected\"":null) . '>' . htmlentities($key) . '</option>';
-							}
-							echo '</select>';
-							echo '<select name="quantiteform">';
-							// for ($i=1; $i < 5; $i++) {
-							for ($i=1; $i < $produitChoisi[$prodcommande]+1; $i++) {
-							// for ($i=$produitChoisi[$prodcommande]+1; $i > 0; $i--) {
-								// $i = 5;
-								echo '<option value="' .htmlentities($i). '" ' . '>' . htmlentities($i) . '</option>';
-								// echo '<option value="' .$i. '" ' . ((isset($i) && $i == $produitChoisi[$prodcommande])?" selected=\"selected\"":null) . '>' . $i . '</option>';
-							}
-							// echo '<option value=' . $produitChoisi[$prodcommande] . '>' . $produitChoisi[$prodcommande] . '</option>';
-							echo '</select>';
+						<?php
+						echo '<select name="' . urlencode('nomform') . '" onchange="document.forms[\'commande\'].submit();">';
+						echo '<option value="-1">- - - Choisissez une produit - - -</option>';
+						foreach($produitChoisi as $key => $value)
+						{
+							echo '<option value="' . htmlentities($key) . '" ' . ((isset($prodcommande) && $prodcommande == $key)?" selected=\"selected\"":null) . '>' . htmlentities($key) . '</option>';
+						}
+						echo '</select>';
+						echo '<select name="quantiteform">';
+						for ($i=1; $i < $produitChoisi[$prodcommande]+1; $i++) {
+							echo '<option value="' .htmlentities($i). '" ' . '>' . htmlentities($i) . '</option>';
+						}
+						echo '</select>';
+						?>
 
-							// echo '<select name="quantite">';
-							// foreach($produitChoisi as $key => $value)
-							// {
-							// 	echo '<option value = ' . $value. '>' .$value. '</option>';
-							// }
-							// echo '</select>';
-							?>
-
-							<br /><input type="submit" name="ok" id="ok" value="Sélectionner" />
-						</fieldset>
-					</form>
-
-
-				<!-- <form action="< ?php echo $url_page ?>" method="post">
-					<p><label for="nom">Nom du produit: </label><input type="text" name="nom" id="nom" value="< ?php if (isset($nom)) { echo htmlentities($nom); } ?>" />
-						<label for="quantite">Quantité max: </label><input type="text" name="quantite" value="< ?php if (isset($quantite)) { echo htmlentities($quantite); } ?>" />
-						<input type="submit" />
-					</p>
-				</form> -->
-					<?php
-					// echo '<select name="nom">';
-					// foreach($produitChoisi as $key => $value)
-					// {
-					// 	echo '<option value = ' . $key. '>' .$key. '</option>';
-					// }
-					// echo '</select>';
-					//
-					// echo '<select name="quantite">';
-					// foreach($produitChoisi as $key => $value)
-					// {
-					// 	echo '<option value = ' . $value. '>' .$value. '</option>';
-					// }
-					// echo '</select>';
-
-					/*
-					* partie à effacer -> fonctionnement avant l'introduction du contrôle du temps
-					*
-					else //Les clients
-					{
-					// Affichage contenu de la table produits commandables
-					echo '<p><strong>Lise des produits commandables:</strong ></p>';
-					echo '<table border=1>';
-					echo '<tr><th bgcolor = \"#CCCCFF\">nom du produit</td>
-					<th bgcolor = \"#CCCCFF\">prix unitaire</td>
-					<th bgcolor = \"#CCCCFF\">nombre à disposition</td>
-					<tr>';
-					while ( $produit = $result->fetch(PDO::FETCH_ASSOC))
-					{
-					echo '<tr><td>' . htmlentities($produit['nom']) .
-					'</td><td>' . htmlentities($produit['prix']) .
-					'</td><td>' . htmlentities($produit['quantite']) .
-					//'</td><td>' . htmlentities($produit['quantite restante']) .
-
-					'</td></tr>';
+						<br /><input type="submit" name="ok" id="ok" value="Sélectionner" />
+					</fieldset>
+				</form>
+				<?php
+//Début de la partie qui pourrait être placée avant le HTML, dans la partie client?
+				if (isset($_SESSION['commandes'])) {
+					$commandes = $_SESSION['commandes'];
 				}
-				echo '</table>';
-				* fin partie à effacer
-				*/
+
+				// Suppression de la ligne sélectionnée de l'array des commandes en cours
+				if (isset($_GET['supprimer']) && is_scalar($_GET['supprimer']))
+				{
+					unset($commandes[$_GET['supprimer']]);
+					$_SESSION['commandes'] = $commandes;
+				}
+
+				if(isset($_POST['ok']) && isset($_POST['nomform']) && isset($_POST['quantiteform']))
+				{
+					$currentTime = (int) date('Gis');
+					$commandes[$currentTime] = array("produit" => $_POST["nomform"],
+																					 "quantite" => $_POST["quantiteform"]);
+					// sauvegarde dans la Session et affichage sous forme de tableau
+					$_SESSION['commandes'] = $commandes;
+				} // mis fin du if ici
+//Fin de la partie qui pourrait être placée avant le HTML, dans la partie client?
+					echo "<table border=1\"px\">";
+					echo '<tr><th bgcolor = \"#CCCCFF\">TimeStamp</td>
+					<th bgcolor = \"#CCCCFF\">Nom du produit</td>
+					<th bgcolor = \"#CCCCFF\">Quantité</td>
+					<th bgcolor = \"#CCCCFF\">action</td>
+					<tr>';
+					foreach ($commandes as $TimeStamp => $commande) {
+						echo "<tr><td>" . htmlentities($TimeStamp) . "</td>";
+						foreach ($commande as $key => $value) {
+							echo "<td>" . htmlentities($value) . "</td>";
+						}
+						echo "<td>" . a($url_page, "supprimer", array("supprimer" => $TimeStamp)) .
+						'</td>';
+						echo "</tr>";
+					}
+					echo "</table>";
+
+
+					echo "<p>Vous avez commandé " . $quantitecommande . " " . $prodcommande ."</p>";
+
+					print_r ($commandes);
+
+
 
 				// produit possible a commander avec "scroll bar" pour la selection du produit et indication du nombre de produit souhaitée
 
